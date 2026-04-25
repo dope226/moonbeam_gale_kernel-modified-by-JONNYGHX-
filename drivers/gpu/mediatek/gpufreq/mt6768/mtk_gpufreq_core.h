b679b229b4271ed06a9fff4ee508fa61b50e740e
blebleble save ur day <108147789+xxblebleblexx@users.noreply.github.com>
2026-04-25 17:43:29 +0700

    Change GPU DVFS frequency from 1200MHz to 1100MHz

--- drivers/gpu/mediatek/gpufreq/mt6768/mtk_gpufreq_core.h
+++ drivers/gpu/mediatek/gpufreq/mt6768/mtk_gpufreq_core.h
@@ -17,7 +17,7 @@
 /**************************************************
  * MT6768/MT6767 : GPU DVFS OPP table Setting
  **************************************************/
-#define SEG_GPU_DVFS_FREQ0			(1200000)	/* KHz */
+#define SEG_GPU_DVFS_FREQ0			(1100000)	/* KHz */
 #define SEG_GPU_DVFS_FREQ1			(1000000)	/* KHz */
 #define SEG_GPU_DVFS_FREQ2			(975000)	/* KHz */
 #define SEG_GPU_DVFS_FREQ3			(950000)	/* KHz */
